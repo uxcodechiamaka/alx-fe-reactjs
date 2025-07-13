871286767d8b442a91b1be2e8a00e8d6192c3e4c
uxcodechiamaka <chiamakanwankwo557@gmail.com>
2025-07-13 20:11:05 +0000

    my assignment

---
+++ alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+export default function MainContent () {
+    return (
+        <div>
+            <main>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+            </main>
+        </div>
+    ) ;
+}
