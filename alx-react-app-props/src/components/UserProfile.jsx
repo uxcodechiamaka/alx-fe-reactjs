296a320a24ea8769a3e7ce0dc121f2145f54ddec
CareAPPGh24 <infratecheng.intl@gmail.com>
2025-07-20 22:34:57 +0000

    my assignment

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 // src/components/UserProfile.jsx
 import React, { useContext } from 'react';
-import { UserContext } from '../contexts/UserContext';
+import { UserContext } from './UserContext';
 
 const UserProfile = () => {
   const { user } = useContext(UserContext);
