78a5ea5b87cbc34494c26adeec8a4955e3286182
CareAPPGh24 <infratecheng.intl@gmail.com>
2025-07-20 23:59:15 +0000

    my assignment

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ background: '#333', padding: '10px' }}>
+    <nav style={{ background: '#333', padding: '10px', backgroundColor: "#2F4F7F", display: "flex", justifyContent: "center" }}>
       <Link to="/" style={{ margin: '10px', color: '#fff' }}>Home</Link>
       <Link to="/about" style={{ margin: '10px', color: '#fff' }}>About</Link>
       <Link to="/services" style={{ margin: '10px', color: '#fff' }}>Services</Link>
