8b459df3f6d014421d4d8aea1168cd5f2e257d0d
CareAPPGh24 <infratecheng.intl@gmail.com>
2025-07-20 22:50:26 +0000

    my assignment

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -2,12 +2,18 @@
 import React from 'react';
 import UserProvider from './components/UserContext';
 import UserProfile from './components/UserProfile';
+import { UserContext } from './components/UserContext';
+import ProfilePage from './components/ProfilePage';
 
 const App = () => {
   return (
     <UserProvider>
       <UserProfile />
+      <UserContext.Provider>
+        <ProfilePage value='userData ' />
+      </UserContext.Provider>
     </UserProvider>
+   
   );
 };
 
