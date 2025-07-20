399ca1ef5e887a514c4f99a1b033546eb6fdf618
CareAPPGh24 <infratecheng.intl@gmail.com>
2025-07-20 21:08:06 +0000

    my assignment

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,7 +7,7 @@ import Header from './components/Header.jsx'
 import MainContent from './components/MainContent.jsx'
 import Footer from './components/Footer.jsx'
 import UserProfile from './components/UserProfile.jsx'
-import Counter from './Counter.jsx'
+import Counter from './components/Counter.jsx'
 
 
 
