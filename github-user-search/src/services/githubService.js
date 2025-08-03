fbb2c27932fddafaa6b770953522edf98253d945
uxcodechiamaka <chiamakanwankwo557@gmail.com>
2025-08-03 16:38:46 +0000

    my assy

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,10 +1,34 @@
 import axios from 'axios';
+const GIT_HUB_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY 
 
 
-export const fetchUserData = async (username) =>{
+export const fetchUserData = async ({username, location , minRepos}) =>{
+  let query = ""
+
+  if (username) query += `${username} in:login`
+  if (location) query += `location:${location}`
+  if (minRepos) query += `repos:>=${minRepos}`
+
+  const url = `https://api.github.com/search/users?q=${encodeURIComponent(query)}&per_page=10`
+
   try{
-    const response = await axios.get(`https://api.github.com/users/${username}`)
-    return (response.data)
+    
+    const {data} = await axios.get(url, {
+      headers:{
+        Authorization: `token ${GIT_HUB_KEY}`
+      }
+    })
+
+    const detailedUsers = await Promise.all(
+      data.items.map(async (user) => {
+        const {data: detail} = await axios.get(user.url, {
+          headers:{
+            Authorization: `token ${GIT_HUB_KEY}`
+          }
+        })
+      })
+    )
+    return (detail)
   }catch(error){
     console.error(error)
   }
