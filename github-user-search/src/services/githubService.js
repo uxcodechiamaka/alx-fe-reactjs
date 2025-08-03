36e39294a0b6e5fdf8feb4c21035b62554320015
uxcodechiamaka <chiamakanwankwo557@gmail.com>
2025-08-03 16:25:59 +0000

    my assy

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,15 +1,13 @@
-export const searchUsers = async ({ username, location, minRepos }) => {
-  let query = username || '';
+import axios from 'axios';
 
-  if (location) {
-    query += `+location:${location}`;
+
+export const fetchUserData = async (username) =>{
+  try{
+    const response = await axios.get(`https://api.github.com/users/${username}`)
+    return (response.data)
+  }catch(error){
+    console.error(error)
   }
-
-  if (minRepos) {
-    query += `+repos:>=${minRepos}`;
-  }
-
-  const response = await fetch(`https://api.github.com/search/users?q=${query}`);
-  const data = await response.json();
-  return data.items; // contains the array of user results
 }
+
+
