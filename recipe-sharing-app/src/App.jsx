829a39ab8b7764a9888d46ba43658bf2df91bada
uxcodechiamaka <chiamakanwankwo557@gmail.com>
2025-08-03 05:12:58 +0000

    task done

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,10 +3,12 @@ import SearchBar from '../components/SearchBar';
 import RecipeList from '../components/RecipeList';
 import FavoritesList from '../components/FavoritesList';
 import RecommendationsList from '../components/RecommendationsList';
+import AddRecipeForm from './components/AddRecipeForm';
 
 const Home = () => {
   return (
     <div>
+      <AddRecipeForm />
       <SearchBar />
       <RecipeList />
       <FavoritesList />
